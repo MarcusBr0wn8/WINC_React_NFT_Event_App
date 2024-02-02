4cf00f5e497800544c75c16a231577c4882f63d8
Marcus Brown <marco@browndesign.nl>
2024-02-02 17:22:17 +0100

    changed db

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -7,17 +7,20 @@ export const loader = async () => {
   try {
     const event = await (
       await fetch(
-        "https://my-json-server.typicode.com/MarcusBr0wn8/event-db/events"
+        // "https://my-json-server.typicode.com/MarcusBr0wn8/event-db/events"
+        "https://my-json-server.typicode.com/MarcusBr0wn8/WINC_React_NFT_Event_App/events"
       )
     ).json();
     const categories = await (
       await fetch(
-        "https://my-json-server.typicode.com/MarcusBr0wn8/event-db/categories/"
+        // "https://my-json-server.typicode.com/MarcusBr0wn8/event-db/categories/"
+        "https://my-json-server.typicode.com/MarcusBr0wn8/WINC_React_NFT_Event_App/categories"
       )
     ).json();
     const users = await (
       await fetch(
-        "https://my-json-server.typicode.com/MarcusBr0wn8/event-db/users"
+        // "https://my-json-server.typicode.com/MarcusBr0wn8/event-db/users"
+        "https://my-json-server.typicode.com/MarcusBr0wn8/WINC_React_NFT_Event_App/users"
       )
     ).json();
 
