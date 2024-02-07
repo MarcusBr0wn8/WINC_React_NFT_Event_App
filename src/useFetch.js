454b590b680d72fb8ae6be0ba2433c83442591ad
Marcus <98151696+MarcusBr0wn8@users.noreply.github.com>
2024-02-07 14:57:13 +0100

    Update useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,33 +1,64 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-//Fetch for EventsPage
+// //Fetch for EventsPage
+// export const useFetch = (url) => {
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState("");
+
+//   useEffect(() => {
+//     let ignore = false;
+//     setData([]);
+//     setIsLoading(true);
+//     setError("");
+//     const fetchData = async () => {
+//       const response = await fetch(url);
+//       if (response.ok) {
+//         const data = await response.json();
+//         if (!ignore) {
+//           setData(data);
+//           setIsLoading(false);
+//         }
+//       } else {
+//         setError(`Something went wrong: ${response.statusText}`);
+//         setIsLoading(false);
+//       }
+//     };
+//     fetchData();
+//     return () => {
+//       ignore = true;
+//     };
+//   }, []);
+//   return { data, isLoading, error };
+// };
+
+import { useState, useEffect } from "react";
+
 export const useFetch = (url) => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [data, setData] = useState(null); // Corrected the type
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null); // Corrected the type
 
   useEffect(() => {
-    let ignore = false;
-    setData([]);
-    setIsLoading(true);
-    setError("");
-    const fetchData = async () => {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        if (!ignore) {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Error fetching users data");
+          }
+          return res.json();
+        })
+        .then((data) => {
           setData(data);
-          setIsLoading(false);
-        }
-      } else {
-        setError(`Something went wrong: ${response.statusText}`);
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-    return () => {
-      ignore = true;
-    };
-  }, []);
-  return { data, isLoading, error };
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, isPending, error };
 };
